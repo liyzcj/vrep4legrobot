c34fe38737cecd2758531f74c66545070d7a5f80
liyzcj <liyz0912@gmail.com>
2018-06-12 00:28:49 +0300

    remove useless code

--- robot/controller.py
+++ robot/controller.py
@@ -1,10 +1,7 @@
 __author__ = 'li yan zhe'
 
 import time
-import math
-import robotdata
 from trotgait import Trotgait
-from transform import rotate
 from keylistener import Keylistener
 
 
@@ -19,62 +16,12 @@ class Controller:
         self.keyListener = Keylistener()
         self.startTime = time.time()
         self.trotgait = Trotgait(self.robot)
-        # self.trotgait.reset()
-
-    def keep_feet_horizontal(self):
-        self.robot.read_imu()
-        # print "orientation:", self.orientation
-        roll = self.robot.orientation[1]
-        # pitch = self.robot.orientation[0]
-        # sin = math.sin(math.radians(pitch))
-        # self.move_leg_to_point('front_left', 80, 75, -50 + sin*30)
-        # self.move_leg_to_point('front_right', -(80), 75, -50 - sin*30)
-        self.robot.move_leg_to_point('front_left', * rotate([60, 75, -50], 'y', - math.radians(roll)))
-        self.robot.move_leg_to_point('front_right', * rotate([-60, 75, -50], 'y', - math.radians(roll)))
-
-    # def start(self):
-    #     """
-    #     setup everything before main loop.
-    #     """
-    #     self.robot.start()
-
-    def run(self):
-        """
-        main thread when running standalone
-        :return:
-        """
-        for i in range(1, 1000):
-            self.iterate()
 
     def trot(self):
         """
         executes a step of the "trot" gait.
         """
         self.trotgait.iterate([self.dx, self.dy, self.dz], self.dro)
-
-    def move_legs_to_offset(self, offset):
-        """
-        move all legs to the same offset
-        """
-        self.move_legs_to_offsets(offset, offset, offset, offset)
-
-    def move_legs_to_offsets(self, front_left_offset, front_right_offset, rear_left_offset, rear_right_offset):
-        """
-        moves each leg to specified offset
-        """
-
-        fl_rest, fr_rest, rr_rest, rl_rest = robotdata.legs_resting_positions
-
-        self.robot.move_leg_to_point('front_left', *(fl_rest + front_left_offset))
-        self.robot.move_leg_to_point('front_right', *(fr_rest + front_right_offset))
-        self.robot.move_leg_to_point('rear_left', *(rl_rest + rear_left_offset))
-        self.robot.move_leg_to_point('rear_right', *(rr_rest + rear_right_offset))
-
-    def move_legs_to_angles(self, a, b, c):
-        """
-        attempt to move all legs to the same angles [a, b, c] (shoulder, femur, tibia)
-        """
-        self.robot.move_legs_to_angles([a, b, c])
 
     def iterate(self):
         """
